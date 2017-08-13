29fdd38c491c7eccc0ae3ec9439d2865ab96528a
John Gibson <jonathangibson02@gmail.com>
2017-08-13 03:21:20 -0400

    Add content

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp'),
+    webserver = require('gulp-webserver'),
+    sass = require('gulp-sass'),
+    mustache = require('gulp-mustache-inverted');
+
+var src = './src',
+    build = './build';
+
+gulp.task('html', function() {
+    gulp.src(build + '/**/*.html');
+});
+
+gulp.task('css', function() {
+    gulp.src(build + '/**/*.css');
+});
+
+gulp.task('json', function() {
+    gulp.src(src + '/**/*.json');
+});
+
+gulp.task('sass', function() {
+    gulp.src(src + '/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(build + '/css/'));
+});
+
+gulp.task('mustache', function() {
+    gulp.src(src + '/**/*.mustache')
+        .pipe(mustache(src + '/data.json', {extension: '.html'}))
+        .pipe(gulp.dest(build + '/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(build + '/**/*.html', ['html']);
+    gulp.watch(build + '/**/*.css', ['css']);
+    gulp.watch(src + '/**/*.json', ['json', 'mustache']);
+    gulp.watch(src + '/**/*.scss', ['sass']);
+    gulp.watch(src + '/**/*.mustache', ['mustache']);
+});
+
+gulp.task('webserver', function() {
+    gulp.src(build  + '/')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
+});
+
+gulp.task('default', ['watch', 'html', 'css', 'json', 'sass', 'mustache', 'webserver']);
