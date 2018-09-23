0e322e9a92990fa64d41b61f71058773f2b57dd1
John Gibson <jonathangibson02@gmail.com>
2018-09-23 13:06:19 -0400

    ESLint checking on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,49 @@
-var gulp = require('gulp'),
-    webserver = require('gulp-webserver'),
-    sass = require('gulp-sass'),
-    mustache = require('gulp-mustache-inverted'),
-    download = require('gulp-download'),
-    moment = require('moment'),
-    fs = require('fs');
+const gulp = require('gulp');
+const webserver = require('gulp-webserver');
+const sass = require('gulp-sass');
+const mustache = require('gulp-mustache-inverted');
+const moment = require('moment');
+const fs = require('fs');
 
-var src = './src',
-    build = './build';
+const src = './src';
+const build = './build';
 
-gulp.task('html', function() {
-    gulp.src(build + '/**/*.html');
+gulp.task('html', () => {
+  gulp.src(`${build}/**/*.html`);
 });
 
-gulp.task('css', function() {
-    gulp.src(build + '/**/*.css');
+gulp.task('css', () => {
+  gulp.src(`${build}/**/*.css`);
 });
 
-gulp.task('sass', function() {
-    gulp.src(src + '/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(build + '/css/'));
+gulp.task('sass', () => {
+  gulp.src(`${src}/**/*.scss`)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(`${build}/css/`));
 });
 
-gulp.task('mustache', function() {
-    let resumeData = JSON.parse(fs.readFileSync(src + '/resume-data.json', "utf8"));
-    resumeData.date = moment().format('MMMM D, YYYY');
-    gulp.src(src + '/**/*.mustache')
-        .pipe(mustache(resumeData, {extension: '.html'}))
-        .pipe(gulp.dest(build + '/'));
+gulp.task('mustache', () => {
+  const resumeData = JSON.parse(fs.readFileSync(`${src}/resume-data.json`, 'utf8'));
+  resumeData.date = moment().format('MMMM D, YYYY');
+  gulp.src(`${src}/**/*.mustache`)
+    .pipe(mustache(resumeData, { extension: '.html' }))
+    .pipe(gulp.dest(`${build}/`));
 });
 
-gulp.task('watch', function() {
-    gulp.watch(src + '/**/*.mustache', ['mustache']);
-    gulp.watch(src + '/**/*.json', ['mustache']);
-    gulp.watch(build + '/**/*.css', ['css']);
-    gulp.watch(src + '/**/*.scss', ['sass']);
+gulp.task('watch', () => {
+  gulp.watch(`${src}/**/*.mustache`, ['mustache']);
+  gulp.watch(`${src}/**/*.json`, ['mustache']);
+  gulp.watch(`${build}/**/*.css`, ['css']);
+  gulp.watch(`${src}/**/*.scss`, ['sass']);
 });
 
-gulp.task('webserver', function() {
-    gulp.src(build  + '/')
-        .pipe(webserver({
-            livereload: true,
-            open: true,
-            port: 8081
-        }));
+gulp.task('webserver', () => {
+  gulp.src(`${build}/`)
+    .pipe(webserver({
+      livereload: true,
+      open: true,
+      port: 8081,
+    }));
 });
 
 gulp.task('default', ['watch', 'html', 'css', 'sass', 'mustache', 'webserver']);
